215ab18f89fcecc4b385440673cac6cd26cc658c
Waga Odongo <wagaodongo@gmail.com>
2019-01-20 10:54:17 +0300

    added
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+
 import './styles.css';
 import NavBar from './components/NavBar';
 import AstronomyList from './components/AstronomyList';
